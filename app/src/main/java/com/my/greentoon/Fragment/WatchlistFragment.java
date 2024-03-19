2442f83374f16b588b4c21b54423f5d3b684a614
Le Trong Nghia <143071139+YouAreMyHome@users.noreply.github.com>
2024-03-19 12:53:29 +0700

    a seng dep try

--- app/src/main/java/com/my/greentoon/Fragment/WatchlistFragment.java
+++ app/src/main/java/com/my/greentoon/Fragment/WatchlistFragment.java
@@ -9,11 +9,11 @@ import androidx.fragment.app.Fragment;
 
 import com.my.greentoon.R;
 
-public class CategoryFragment extends Fragment {
+public class WatchlistFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        return inflater.inflate(R.layout.fragment_watchlist, container, false);
     }
 }
