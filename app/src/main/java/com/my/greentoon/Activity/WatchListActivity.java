3c5533a369195fc252c74884e299d7b72f4fa71d
Le Trong Nghia <143071139+YouAreMyHome@users.noreply.github.com>
2024-03-19 13:14:51 +0700

    xamvl

---
+++ app/src/main/java/com/my/greentoon/Activity/WatchListActivity.java
@@ -0,0 +1,16 @@
+package com.my.greentoon.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.my.greentoon.R;
+
+public class WatchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watchlist);
+    }
+}
